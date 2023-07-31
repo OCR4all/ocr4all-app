4cf4032d853974ba40af768b6a11eadbf56858f3
Herbert Baier <baier@itbaier.de>
2023-07-31 13:49:32 +0200

    cors/added for desktop profile

--- src/main/java/de/uniwuerzburg/zpd/ocr4all/application/api/security/ApiSecurityDesktopConfig.java
+++ src/main/java/de/uniwuerzburg/zpd/ocr4all/application/api/security/ApiSecurityDesktopConfig.java
@@ -10,11 +10,9 @@ package de.uniwuerzburg.zpd.ocr4all.application.api.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-
-import de.uniwuerzburg.zpd.ocr4all.application.core.security.SecurityConfig;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 
 /**
  * Disables Spring Security for desktop profiles and defines CORS filters.
@@ -26,19 +24,33 @@ import de.uniwuerzburg.zpd.ocr4all.application.core.security.SecurityConfig;
  */
 @Profile("api & desktop")
 @Configuration
-public class ApiSecurityDesktopConfig extends SecurityConfig {
+public class ApiSecurityDesktopConfig extends ApiSecurityConfig {
 	/**
-	 * Customizes {@link WebSecurity}. Beans of this type will automatically be used
-	 * by {@link WebSecurityConfiguration} to customize {@link WebSecurity}.
+	 * Configures the {@code FilterChainProxy}.
 	 * 
-	 * @return The customizations to perform on {@link WebSecurity}.
+	 * @param http The {@link HttpSecurity} is similar to Spring Security's XML
+	 *             &lt;http&gt; element in the namespace configuration. It allows
+	 *             configuring web based security for specific http requests. By
+	 *             default it will be applied to all requests, but can be restricted
+	 *             using {@link #requestMatcher(RequestMatcher)} or other similar
+	 *             methods.
+	 * @return The filter chain which is capable of being matched against an
+	 *         {@code HttpServletRequest} in order to decide whether it applies to
+	 *         that request.
+	 * @throws Exception Throws on filter chain exceptions.
 	 * @since 17
 	 */
 	@Bean
-	WebSecurityCustomizer webSecurityCustomizer() {
-		return (web) -> {
-			web.ignoring().requestMatchers(patternMatchZeroMoreDirectories);
-		};
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		// enable CORS and disable CSRF
+		http.cors(cors -> cors.configurationSource(corsConfigurationSource())).csrf(csrf -> csrf.disable());
+
+		/*
+		 * specify that all URLs are allowed by anyone
+		 */
+		http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+
+		return http.build();
 	}
 
 }
